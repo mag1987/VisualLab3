fed25c679d6b2b1fb38f0492da5a509154bd6da5
mag1987 <mag987@list.ru>
2019-02-04 08:20:34 +0700

    добавлено удаление изменение поиск

--- VisualLab3/Controllers/OrganizationController.cs
+++ VisualLab3/Controllers/OrganizationController.cs
@@ -10,42 +10,54 @@ namespace VisualLab3.Controllers
 {
     public class OrganizationController : ApiController
     {
-        Organization[] organizations = new Organization[]
+        static List<Organization> organizations = new List<Organization>
         {
             new Organization { Name = "Иванов и сыновья", Inn = 123456798, Phone = 1112223 },
             new Organization { Name = "Молодые ветра", Inn = 45454545, Phone = 2221113 },
             new Organization { Name = "Красные тюльпаны", Inn = 555555555, Phone = 4444447 },
-        };
 
+        };
+        [HttpGet]
         public IEnumerable<Organization> GetAllOrganizations()
         {
             return organizations;
         }
-
-        public IEnumerable<Organization> GetOrganizations1(ulong Name)
+        [HttpGet]
+        public IEnumerable<Organization> GetOrganizations(string Name)
         {
             var organization = from org in organizations
-                               where org.Phone == Name
-                               select org;
-                           
+                               where org.Name == Name
+                               select org;                  
             return organization;
         }
-
-        public IHttpActionResult GetOrganizations(string Name)
+        [HttpGet]
+        public void AddOrganization(string name, ulong inn, ulong phone)
         {
-            var organization = organizations[0];
-                /*
-                from org in organizations
-                               where org.Name == Name
-                               select org;
-                               */
-            if (organization == null)
-            {
-                return NotFound();
-            }
-            return Ok(organization);
+            organizations.Add(new Organization
+            { Name = name, Inn= inn, Phone= phone } );
         }
-        //public IHttpActionResult AddOrganization()
-        //{ }
+        [HttpGet]
+        public void ChangeOrganization(string name, ulong inn, ulong phone)
+        {
+            var organization = from org in organizations
+                               where org.Name == name
+                               select org;
+            if (organization.Count() == 1)
+            {
+                organization.First().Inn = inn;
+                organization.First().Phone = phone;
+            }
+        }
+        [HttpGet]
+        public void DeleteOrganization(string name, ulong inn, ulong phone)
+        {
+            var organization = from org in organizations
+                               where org.Name == name
+                               select org;
+            if (organization.Count() == 1)
+            {
+                organizations.Remove(organization.First());
+            }
+        }
     }
 }
